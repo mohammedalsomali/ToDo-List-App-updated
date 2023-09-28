e1b22856d423d6fe48cf8aa6cc5161ab6e11a4d1
mohammedalsomali <d.j.hamoody@gmail.com>
2023-09-28 10:21:56 -0500

    new way to do the todo list

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
-import ListCreation from "./ListCreation";
 import React from "react";
-import { useEffect, useRef, useState } from "react";
+import SideNav from "./SideNav";
+// import ListCreation from "./ListCreation";
+// import { useEffect, useRef, useState } from "react";
 
-function App() {
-  //const currentinput = useRef([]);
-  const [currentValue, SetValue] = useState({ 
-    input1 : "lk",
-    input2 : "lkj"
-  });
-  const [Todo, SetTodos] = useState({
-    val : "llklkl",
-    value: "lklkm"
-  });
-
-  function UpdateTodo(e) {
-    e.preventDefault()
-
-
-    
-      // SetTodos(currentTodos => {
-      //   return [...currentTodos, { id: crypto.randomUUID(), val: currentValue.input1, value: currentValue.input2 }]
-      // })
-
-    
-    
-  }
-
-  console.log(["todo ", Todo], ["val ", currentValue]);
+function App(props, state) {
+  
   return (
-    <>
-      <form onSubmit={UpdateTodo}>
-        <input
-          type="text"
-          value={currentValue.input1}
-          onChange={(e) => SetValue({...Todo, val: e.target.value})}
-        />
-        <input 
-        type="text"
-        value={currentValue.input2}
-        onChange={(e) => SetValue({...Todo, value: e.target.value})}
-        />
-        <button className="Addbtn" type="submit" />
-      </form>
-
-      <ListCreation todos={Todo} />
-    </>
-  );
+    <div className="container">
+      <SideNav =  />
+    </div>
+    
+  )
+    
 }
 
 export default App;
---
+++ src/SideNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const SideNav = (props) => {
+return (
+   <div className="sideNav" style={{ width: '25%', paddingTop: '20px' }}>
+      <a >About</a>
+      <a >Services</a>
+      <a >Clients</a>
+      <a >Contact</a>
+   </div>
+ )
+}
+export default SideNav;
