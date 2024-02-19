58f2156ee8a2b9d02cf8c14ebfd339400e279b1b
mohammedalsomali <d.j.hamoody@gmail.com>
2024-02-19 09:38:18 -0600

    removed jquery

--- src/Todo_List/calendar.js
+++ src/Todo_List/calendar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Calendar from "react-calendar";
 import jQuery from "jquery";
 import 'react-calendar/dist/Calendar.css'
@@ -6,6 +6,9 @@ import 'react-calendar/dist/Calendar.css'
 
 
 export default function Calndr(){
+
+    
+
     return (
         <Calendar className="calendar"/>
     )
--- src/Todo_List/style.js
+++ src/Todo_List/style.js
@@ -1,22 +1,6 @@
-import React from "react";
-import jQuery from "jquery";
-import Calendar from "react-calendar";
-
-
-const content = $(".react-calendar__tile abbr").text();
-
-    if (content == "1") {
-        console.log(content);
-        $(".react-calendar__tile").css("color", "#ffffff");
-    }
-   if (content == "2") {
-
-        $(".react-calendar__tile").css("color", "#ccc");
-    }
-   if (content == "3") {
-
-        $(".react-calendar__tile").css("color", "#000");
-    }
-
-
-export default content;
+// import React from "react";
+// import Calendar from "react-calendar";
+// export default function setTileColor(){
+//     var tileText = document.querySelectorAll("abbr");
+//     for(i = 0; )
+// }
