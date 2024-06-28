84371423f40fb0d5219461cc8b6ab54b51843cd6
mohammedalsomali <d.j.hamoody@gmail.com>
2024-06-28 09:34:56 -0500

    messing a round with transitions

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ var formSubject = document.querySelector('.subject');
 var todoDiscription= document.querySelector('.discription');
 var userEmail = document.querySelector('.email');
 var dueDate = document.querySelector('.dueDate');
+var trashcontainer = document.querySelector('.trash');
 
 window.onload = function () {
     
     document.querySelector('.startTodoBtn').addEventListener('click', openForm);
+    document.querySelector('.trashBtn').addEventListener('click', showTrash);
     
     
 } 
@@ -19,15 +21,37 @@ function openForm (){
     startForm.style.opacity = 0;
     startForm.style.height = '0px';
     startForm.style.width = '0px';
-    
-
 
     formSubject.value = todoSubject.value;
 
-
     todoForm.style.height = '60%';
-   
-
     
 }
+
+
+
+
+function showTrash() {
+
+    if(trashcontainer.style.height == '' || trashcontainer.style.height == '0px') {
+        startForm.style.visibility = 'hidden';
+        startForm.style.opacity = 0;
+        startForm.style.height = '0px';
+        todoForm.style.height = '0px';
+        trashcontainer.style.height = '60%'
+        return
+    }    
+
+
+    trashcontainer.style.height = '0px';
+    startForm.style.visibility = 'visible';
+    startForm.style.opacity = 1;
+    startForm.style.height = '40px';
+
+}
+
+function submittodoForm() {
+    
+}
+
 
